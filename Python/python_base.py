c1a760756fb34358fc1b0236e78672413fe414ec
yxf94023 <1594149156@qq.com>
2017-04-03 01:15:04 +0800

    保存代码

--- Python/python_base.py
+++ Python/python_base.py
@@ -2,6 +2,21 @@
 # -*- coding: UTF-8 -*-
 
 ##
+#\mainpage Python语言
+#
+#\section Python的数据类型
+#
+#	This is.
+#
+#\setcion Python的条件语句
+#
+
+##
+#\defgroup python_var_type Python的数据类型
+#@{
+#
+
+##
 #\brief 变量类型，输出如下
 #
 #	100<br/>
@@ -75,3 +90,85 @@ p_string_operator_fun()
 p_list_operator_fun()
 
 p_dict_operator_fun()
+
+##
+#@}
+#
+
+##
+#\defgroup python_condition_command Python的条件语句
+#@{
+#
+
+##
+#\brief 条件语句
+#
+#	- 任何非0和非空（null）值为true，0或者null为false
+#
+def p_if_operator():
+	flag = 5
+	"单个的if语句"
+	if (flag == 5):
+		print "flag is five"
+	"if/else"
+	if (flag == 4):
+		print "flag is four"
+	else:
+		print "flag is not four"
+	"if/elif"
+	if (flag == 4):
+		print "flag is four"
+	elif (flag == 6):
+		print "flag is six"
+	elif (flag == 5):
+		print "flag is five"
+	else:
+		print "flag is other values"
+	return
+	
+p_if_operator()
+##
+#@}
+#
+
+##
+#\defgroup python_loop_statement Python的循环语句
+#@{
+#
+
+##
+#\brief 循环语句操作
+#
+#	- Python提供了 for 和 while 循环 没有do...while循环
+#		- 在python中， while ... else 在循环条件为false时执行else语句
+#
+def p_loop_opertator():
+	numbers = [12, 37, 5, 4]
+	even = []
+	odd = []
+	while len(numbers) > 0:
+		number = numbers.pop()
+		if (number % 2 == 0):
+			even.append(number)
+		else:
+			odd.append(number)
+	else:
+		print "while loop is over"
+	"输出结果"
+	print "\teven:", even
+	print "\todd:", odd
+	names = ['y', 'yx', 'yxf']
+	for name in names:
+		print name
+	for idx in range(len(names)):
+		print names[idx]
+	else:
+		print "for loop is over"
+	return
+	
+p_loop_opertator()
+
+##
+#@}
+#
+
